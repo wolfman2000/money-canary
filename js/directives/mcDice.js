a37f43399c6b41aa890c0bb89e4ff289e2034325
Jason Felds <jafelds@gmail.com>
2013-12-11 22:06:33 -0500

    Formatting.

--- js/directives/mcDice.js
+++ js/directives/mcDice.js
@@ -9,37 +9,37 @@ var dice = angular.module('mcDice',[])
 dice.factory('simpleRngFactory', SimpleRngFactory);
 
 dice.directive('simpleDice', ['$compile','simpleRngFactory',function($compile,simpleRngFactory){
-	// Runs during compile
-	return {
-		// name: '',
-		// priority: 1,
-		// terminal: true,
-		 scope: {
-		 	diceOptions : '=',
-		 	ngModel : '='
+  // Runs during compile
+  return {
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+     scope: {
+      diceOptions : '=',
+      ngModel : '='
 
-		 }, // {} = isolate, true = child, false/undefined = no change
-		 controller : function($scope, $element, $attrs, $transclude) {
+     }, // {} = isolate, true = child, false/undefined = no change
+     controller : function($scope, $element, $attrs, $transclude) {
 
-		 	$scope.rollDice = function(){
+      $scope.rollDice = function(){
 
-		 		$scope.diceDisplay = simpleRngFactory.random()
-		 		console.log($scope.diceDisplay)
+        $scope.diceDisplay = simpleRngFactory.random()
+        console.log($scope.diceDisplay)
 
-		 	}
+      }
 
 
 
-		 },
-		 require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-		 restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
-		 template: '<div><button ng-click="rollDice()">Roll Dice</button> {{diceDisplay}}</div>',
-		// templateUrl: '',
-		 replace: true,
-		// transclude: true,
-		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-		 link: function($scope, iElm, iAttrs, controller) {
-			
-		 }
-	};
+     },
+     require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+     restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
+     template: '<div><button ng-click="rollDice()">Roll Dice</button> {{diceDisplay}}</div>',
+    // templateUrl: '',
+     replace: true,
+    // transclude: true,
+    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+     link: function($scope, iElm, iAttrs, controller) {
+      
+     }
+  };
 }]);
